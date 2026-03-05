a62649db4696dcc04a744d350c5d906f550f0f75
Kushal <kushalkhanal181@gmail.com>
2026-03-05 11:22:19 +0545

    feat: Initialize full-stack real estate application with Vite client, Express server, MongoDB connection, and sitemap.

--- server/server.js
+++ server/server.js
@@ -18,55 +18,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'Run Real Estate API is running 🏡' });
 });
 
-// Dynamic Sitemap
-app.get('/sitemap.xml', async (req, res) => {
-  try {
-    const Plot = require('./models/Plot');
-    const plots = await Plot.find({});
-
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <!-- Static Pages -->
-  <url>
-    <loc>https://runrealestate.com.np/</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://runrealestate.com.np/about</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://runrealestate.com.np/contact</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-`;
-
-    // Dynamic Plot Pages
-    plots.forEach(plot => {
-      // Use slugified title or ID
-      const slug = plot._id || plot.title.replace(/\s+/g, '-').toLowerCase();
-      const lastMod = plot.updatedAt ? new Date(plot.updatedAt).toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
-
-      xml += `  <url>
-    <loc>https://runrealestate.com.np/plot/${slug}</loc>
-    <lastmod>${lastMod}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>\n`;
-    });
-
-    xml += `</urlset>`;
-
-    res.header('Content-Type', 'application/xml');
-    res.status(200).send(xml);
-  } catch (err) {
-    console.error('Sitemap generation error:', err);
-    res.status(500).end();
-  }
-});
 
 // Connect to MongoDB
 mongoose
